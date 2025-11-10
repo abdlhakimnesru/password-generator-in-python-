93ecd1a9d4ef24cfb0abc2ad0c19c4af9caa242e
Abdulhakim <abdlhakimnesrugmail.com>
2025-11-10 12:08:35 +0300

    Update app to use Render PORT

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import random
 import string
+import os
 
 app = Flask(__name__)
 
@@ -17,4 +18,5 @@ def home():
     return render_template('index.html', password=password)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Use Render's assigned port
+    app.run(host='0.0.0.0', port=port, debug=True)
